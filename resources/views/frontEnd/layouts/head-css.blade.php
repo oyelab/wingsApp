a7bae53a28232fb8ce9bd4a6d40fe7b58e83f862
Faisal Hasan <me@faisal.one>
2024-10-23 03:44:24 +0600

    Homepage SVG Icons change to Font Awesome Icon

--- resources/views/frontEnd/layouts/head-css.blade.php
+++ resources/views/frontEnd/layouts/head-css.blade.php
@@ -3,4 +3,7 @@
 <link rel="stylesheet" href="{{ asset('frontEnd/css/style.css') }}" />
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+
+<!-- Place your kit's code here -->
+<script src="https://kit.fontawesome.com/06f61db76e.js" crossorigin="anonymous"></script>
 @yield('css')
