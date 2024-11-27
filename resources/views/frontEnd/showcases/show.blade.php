8cfaaa3dbbd3bccc493e84c18f12b78d7e8c8b1b
Faisal Hasan <me@faisal.one>
2024-11-28 04:08:50 +0600

    Showcase fixes

--- resources/views/frontEnd/showcases/show.blade.php
+++ resources/views/frontEnd/showcases/show.blade.php
@@ -1,8 +1,11 @@
 @extends('frontEnd.layouts.app')
+@section('pageTitle', $showcase->title . ' | ')
+@section('pageDescription', $showcase->description)
+@section('pageOgImage', $showcase->bannersImagePath->get(0))  <!-- Image specific to this page -->
 @section('content')
 <div class="container-fluid p-0">
   <div class="row no-gutters m-0 d-flex">
-	@foreach($showcase->banners_image_path as $imagePath)
+	@foreach($showcase->bannersImagePath as $imagePath)
 	<div class="col-12 p-0">
 		<img 
 			src="{{ $imagePath }}"
