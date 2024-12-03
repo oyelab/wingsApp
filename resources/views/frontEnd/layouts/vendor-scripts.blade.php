cc188b163785c82e2d7821590306ad82202aa0eb
Faisal Hasan <me@faisal.one>
2024-12-03 23:55:40 +0600

    Fontawesome library configured

--- resources/views/frontEnd/layouts/vendor-scripts.blade.php
+++ resources/views/frontEnd/layouts/vendor-scripts.blade.php
@@ -1,14 +1,11 @@
 <script src="{{ asset('frontEnd/js/jquery.min.js') }}"></script>
 
 <!-- Place your kit's code here -->
-<!-- <script src="https://kit.fontawesome.com/06f61db76e.js" crossorigin="anonymous"></script> -->
+<script src="https://kit.fontawesome.com/06f61db76e.js" crossorigin="anonymous"></script>
 
 <script src="{{ asset('frontEnd/js/bootstrap.bundle.min.js') }}"></script>
-<!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script> -->
-
 <script src="{{ asset('frontEnd/js/swiper-bundle.min.js') }}"></script>
 <script src="{{ asset('frontEnd/js/main.js') }}"></script>
-<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script> -->
 <script>
     // Optional: This script is to ensure the badge is updated if necessary
     function updateCartCount() {
