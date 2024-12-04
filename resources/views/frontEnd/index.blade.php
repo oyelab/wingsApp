e08a861aea47449092f95df703e5ab607de12b5e
nwebpro <abnaeem.bsc@gmail.com>
2024-12-04 10:48:47 +0600

    home page and footer responsive done for mobile

--- resources/views/frontEnd/index.blade.php
+++ resources/views/frontEnd/index.blade.php
@@ -864,7 +864,7 @@
 					@if($manufactureLogo && $manufactureLogo->filePath)
 						<img
 							src="{{ $manufactureLogo->filePath }}"
-							class="img-fluid w-50"
+							class="img-fluid"
 							alt="Manufacture Logo"
 							draggable="{{ $manufactureLogo->title }}"
 						/>
