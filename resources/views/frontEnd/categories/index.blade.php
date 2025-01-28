b3d63d887c7cd7a03721f8e67c9348e6cc9f71e7
Faisal Hasan <me@faisal.one>
2025-01-28 22:47:43 +0600

    meta title fixed

--- resources/views/frontEnd/categories/index.blade.php
+++ resources/views/frontEnd/categories/index.blade.php
@@ -1,6 +1,5 @@
 @extends('frontEnd.layouts.app')
 
-@section('pageTitle', $pageTitle . ' | ')
 {{-- @section('pageDescription', $showcase->description)
 @section('pageOgImage', $showcase->ogImagePath)  <!-- Image specific to this page --> --}}
 
